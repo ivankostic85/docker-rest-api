349fe5af39e23603278e19f8b7e13173ddd259e6
Ivan Kostic <v-ivankostic@microsoft.com>
2021-08-01 13:12:55 +0200

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,6 @@
+FROM tiangolo/uvicorn-gunicorn-fastapi:python3.7
+
+COPY ./app /app
+
+RUN pip install -r requirements.txt --no-cache-dir
+
---
+++ app/main.py
@@ -0,0 +1,29 @@
+
+from typing import Optional
+
+from fastapi import FastAPI
+
+import docker
+
+app = FastAPI()
+dockerClient = docker.APIClient(base_url='unix://var/run/docker.sock')
+
+
+@app.get("/")
+def info():
+    return "Docker REST API"
+
+@app.get("/status")
+def check_status():
+    return {"Status": "OK"}
+
+
+# @app.get("/items/{item_id}")
+# def read_item(item_id: int, q: Optional[str] = None):
+#     return {"item_id": item_id, "q": q}
+
+@app.get("/docker")
+def docker():
+    dockerVer = dockerClient.version()
+    # print(dockerVer)
+    return dockerVer
